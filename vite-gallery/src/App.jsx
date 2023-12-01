b606d1a3daefa1f1e2cf90a6ffe24f47fc5f772a
Dima <dima.tsymbal12345@gmail.com>
2023-12-01 22:43:14 +0200

    0101

---
+++ vite-gallery/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Catalog from "./Catalog";
+
+const images = [
+  "https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  "https://images.pexels.com/photos/416430/pexels-photo-416430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  "https://images.pexels.com/photos/310452/pexels-photo-310452.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  "https://images.pexels.com/photos/1738986/pexels-photo-1738986.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  "https://images.pexels.com/photos/327482/pexels-photo-327482.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  "https://images.pexels.com/photos/358574/pexels-photo-358574.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+];
+
+function App() {
+  return (
+    <div>
+      <Catalog images={images} />
+    </div>
+  );
+}
+
+export default App;
